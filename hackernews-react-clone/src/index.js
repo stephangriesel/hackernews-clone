c52f942378f50d91e13207df80621cefd8588846
Stephan Griesel <sgriesel@gmail.com>
2018-12-24 08:04:00 +0100

    update app component, add header component & update router index.js

--- hackernews-react-clone/src/index.js
+++ hackernews-react-clone/src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom'
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
@@ -18,11 +19,13 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

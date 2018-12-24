e7516df18e823b6f1d17a9716d084956c32bab7e
Stephan Griesel <sgriesel@gmail.com>
2018-12-24 09:07:18 +0100

    update method linklist

--- hackernews-react-clone/src/components/LinkList.js
+++ hackernews-react-clone/src/components/LinkList.js
@@ -39,7 +39,12 @@ class LinkList extends Component {
             return (
                 <div>
                   {linksToRender.map((link, index) => (
-                    <Link key={link.id} link={link} index={index} />
+                    <Link 
+                        key={link.id} 
+                        link={link} 
+                        index={index} 
+                        updateStoreAfterVote={this._updateCacheAfterVote}
+                        />
                   ))}
                 </div>
               )
@@ -47,6 +52,14 @@ class LinkList extends Component {
         </Query>
       )
     }
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({ query: FEED_QUERY })
+      
+        const votedLink = data.feed.links.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+      
+        store.writeQuery({ query: FEED_QUERY, data })
+      }
   }
 
 export default LinkList
